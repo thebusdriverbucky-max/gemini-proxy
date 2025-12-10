f3bc2434378f0b20c45d324066c8a68610306966
thebusdriverbucky-max <thebusdriverbucky@gmail.com>
2025-12-10 17:09:02 +0200

    gemini downgrade to flash 1.5

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/gemini', async (req, res) => {
   }
 
 
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
 
 
   console.log('Sending request to:', url); // ЛОГИРОВАНИЕ
